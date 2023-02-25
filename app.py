f4fcc854e0eed21a7c47641bbbc1a424254bf049
Xenon1345 <64515978+Xenon1345@users.noreply.github.com>
2023-02-25 18:14:06 +0800

    cli interface frfr on god

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from data_structures import *
 import json, pickle, datetime, requests
 
-wallets = { "mingy": 69, "alice": 200, "bob":5 }
+wallets = { "mingy": 69, "alice": 2000, "bob":5 }
 ledger = []
 app = Flask(__name__)
 
@@ -23,7 +23,8 @@ def transact():
                 "ledger": ledger[-10:]
             }
         elif len(ledger) == 0:
-            return "bruh"
+            # bruh
+            return abort(406)
         else:
             error_ledger = {
                 "head": ledger[0].hash,
@@ -31,18 +32,18 @@ def transact():
             }
         if "source" in data:
             error_ledger["ledger"] = list(map(lambda led: led.deserde(), error_ledger["ledger"]))
-            print(str(error_ledger))
+            print(error_ledger)
             requests.post("http://"+data["source"]+"/error", json=json.dumps(error_ledger))
-        abort(406)
+        return abort(406)
     return "Success"
 
 @app.route("/error", methods=['POST'])
 def error():
     data = request.json
-    hash = data['head']
+    thash = data['head']
     found = False
     for i in range(len(ledger)):
-        if ledger[i].hash == hash:
+        if ledger[i].hash == thash:
             newlist = data['ledger']
             print("\ngetting an error:\n", newlist, '\n\n')
             ledger = ledger[:i+1] + newlist
@@ -51,6 +52,7 @@ def error():
         abort(406) # Not Acceptable
 
 # DEBUG
-@app.route("/query")
+@app.route("/query", methods=["GET"])
 def query():
-    return json.dumps(wallets)
+    user = request.args.get('user')
+    return f"balance: {wallets[user]}" if user in wallets.keys() else abort(406)
--- test/basic.py
+++ test/basic.py
@@ -2,15 +2,64 @@ import requests
 
 outer_nodes = ["127.0.0.1:5000"]
 
-for node in outer_nodes:
-    try:
-        print("Trying node " + node)
-        # Do work
-        data = {"sender":"alice", "recipients": {"bob": 100}, "source": "127.0.0.1:5002"}
-        requests.post("http://"+node+"/transact", json=data)
-        print("Sucess!")
-        break
-    except Exception as e:
-        print(e)
-        print("Trying next node")
+# for node in outer_nodes:
+#     try:
+#         print("Trying node " + node)
+#         # Do work
+#         data = {"sender":"alice", "recipients": {"bob": 100}, "source": "127.0.0.1:5002"}
+#         requests.post(f"http://{node}/transact", json=data)
+#         print("NOT Sucess!")
+#         break
+#     except Exception as e:
+#         print(e)
+#         print("Trying next node")
 
+def main():
+    name = input("who r u: ").strip()
+
+    while(True):
+        print("""
+i love boys
+1) give monie
+2) check monie
+3) NEW name!!!!!!
+""")
+
+        option = input("> ")
+        if option == "1":
+            receipient = input("2 who: ")
+            while True:
+                amount = input("hao muhc: ")
+                try:
+                    amount = int(amount)
+                except Exception as e:
+                    print("that isnt a numbnr,,,,")
+                else:
+                    break
+
+            for node in outer_nodes:
+                try:
+                    print("Trying node " + node)
+                    # Do work
+                    data = {"sender":name, "recipients": {receipient.strip(): amount}}
+                    response = requests.post(f"http://{node}/transact", json=data)
+                    if response.text == "Success":
+                        print("yipee!!!!")
+                        break
+
+                    else:
+                        print(f"u r stupid,,, {response.text}")
+                except Exception as e:
+                    print(e)
+                    print("Trying next node")
+
+        elif option == "2":
+            user = input("who: ")
+            print(requests.get(f"http://{outer_nodes[0]}/query", {"user":user.strip()}).text)
+        elif option == "3":
+            name = input("who r u NOW: ").strip()
+        else:
+            print("u r stupid")
+
+
+main()
