7de514433be9c3c3b1a6e0882304f30476992cf2
spoon <hanxyn888@gmail.com>
2023-02-26 09:04:00 +0800

    imrpove client.py else we die

--- test/client.py
+++ test/client.py
@@ -16,48 +16,49 @@ outer_nodes = ["127.0.0.1:5003"]
 #         print("Trying next node")
 
 def main():
-    filename = input("Where key? ")
-    infile = open(filename+".pub", 'r')
+    username = input("Please enter your username to log in: ")
+    infile = open(username+".pub", 'r')
     pub = infile.read()
     infile.close()
-    infile = open(filename, 'r')
+    infile = open(username, 'r')
     priv = infile.read()
     infile.close()
     private = SigningKey.from_pem(priv)
-    sign = private.sign(b'among us')
+    test_sign = b'among us'
+    sign = private.sign(test_sign)
     public = VerifyingKey.from_pem(pub)
-    if not public.verify(sign, b'among us'):
-        print("ERROR: ecdsa and ecdsa.pub are not a valid key pair.")
+    if not public.verify(sign, test_sign):
+        print(f"ERROR: {username} and {username}.pub are not a valid key pair.")
         return
 
 
     while(True):
         print("""
-i love boys
-1) give monie
-2) check monie
-3) NEW name!!!!!!
-4) exit
+Welcome to T Pay!
+1) Transfer someone money
+2) Check someone's money
+3) Rename your account
+4) Exit
 """)
 
         option = input("> ")
         if option == "1":
             recipients = {}
-            who = input("to who? (empty to finish) ").strip()
+            who = input("To who? (empty to finish) ").strip()
             if who == '':
-                print("thats nobody")
+                print("that's literally nobody")
                 continue
             while who != '':
                 try:
-                    amt = int(input("how much: "))
+                    amt = int(input("How much: "))
                     recipients[who] = amt
                 except:
-                    print("not a number")
-                who = input("to who? (empty to finish) ").strip()
+                    print("not a number bruh")
+                who = input("To who? (empty to finish) ").strip()
 
             try:
                 node = random.choice(outer_nodes)
-                print("Trying node " + node)
+                # print("Trying node " + node)
                 m = hashlib.sha256()
                 m.update(json.dumps({
                     "pubkey": public.to_pem()[27:-26].decode(),
@@ -72,21 +73,21 @@ i love boys
                 }
                 response = requests.post(f"http://{node}/transact", json=data)
                 if response.text == "Success":
-                    print("yipee!!!!")
+                    print("Transaction success!")
                     continue
                 else:
-                    print(f"u r stupid,,, {response.text}")
+                    print(f"something stupid happened: {response.text}")
             except Exception as e:
                 traceback.print_exception(type(e), e, e.__traceback__)
-                print("Trying next node")
+                print("something happened, trying next node")
 
         elif option == "2":
-            user = input("who: ")
+            user = input("Who's account would you like to check: ")
             print(requests.get(f"http://{random.choice(outer_nodes)}/query", {"user":user.strip()}).text)
         elif option == "3":
             private = SigningKey.generate(curve=SECP256k1)
             public = private.verifying_key
-            filename = input("Enter file to write public key to: ")
+            filename = input("Enter your new username: ")
             outfile = open(filename, 'w')
             outfile.write(private.to_pem().decode())
             outfile.close()
@@ -94,9 +95,10 @@ i love boys
             outfile.write(public.to_pem().decode())
             outfile.close()
         elif option == "4":
+            print("Thank you for using T Pay!")
             break
         else:
-            print("u r stupid")
+            print("u r stupid please enter an actually valid option please thank you!!!")
 
 
 main()
